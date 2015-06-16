d4cfb8f180ac9732489aad3c2999da38530ca059
Jason Voegele <jason.voegele@gmail.com>
2015-06-16 10:55:40 -0400

    WIP for C++ `bob` exercise

--- cpp/bob/bob.h
+++ cpp/bob/bob.h
@@ -1,7 +1,21 @@
 #include <string>
+#include <boost/algorithm/string.hpp>
+
+using std::string;
 
 namespace bob {
-  std::string hey(const std::string &input) {
-    return "Whatever.";
+  bool is_shouting(const string &input) {
+    auto upper = boost::to_upper_copy(input);
+    auto lower = boost::to_lower_copy(input);
+    return input == upper && input != lower;
+  }
+
+  string hey(const string &input) {
+    if (is_shouting(input)) {
+      return "Whoa, chill out!";
+    }
+    else {
+      return "Whatever.";
+    }
   }
 }
--- cpp/bob/bob_test.cpp
+++ cpp/bob/bob_test.cpp
@@ -7,7 +7,6 @@ BOOST_AUTO_TEST_CASE(stating_something)
     BOOST_REQUIRE_EQUAL("Whatever.", bob::hey("Tom-ay-to, tom-aaaah-to."));
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
 BOOST_AUTO_TEST_CASE(shouting)
 {
     BOOST_REQUIRE_EQUAL("Whoa, chill out!", bob::hey("WATCH OUT!"));
@@ -18,6 +17,7 @@ BOOST_AUTO_TEST_CASE(asking_a_question)
     BOOST_REQUIRE_EQUAL("Sure.", bob::hey("Does this cryogenic chamber make me look fat?"));
 }
 
+#if defined(EXERCISM_RUN_ALL_TESTS)
 BOOST_AUTO_TEST_CASE(talking_forcefully)
 {
     BOOST_REQUIRE_EQUAL("Whatever.", bob::hey("Let's go make out behind the gym!"));
