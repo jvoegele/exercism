24f7a4b3cc0ce00ed485dd55c04c8bdb4634d344
Jason Voegele <jason.voegele@gmail.com>
2015-11-11 14:00:09 -0800

    Use bsl operator instead of math:pow function

--- erlang/grains/grains.erl
+++ erlang/grains/grains.erl
@@ -1,6 +1,9 @@
 -module(grains).
 -export([square/1, total/0]).
 
-square(N) -> round(math:pow(2, N - 1)).
+square(N) ->
+    %% round(math:pow(2, N - 1)).
+    1 bsl (N - 1).
 
-total() -> square(65) - 1.
+total() ->
+    square(65) - 1.
