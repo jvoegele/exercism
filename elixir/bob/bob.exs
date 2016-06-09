3789c3895b92bbe45a811398895da11b7dbfd885
Jason Voegele <jason.voegele@gmail.com>
2016-06-09 15:52:07 -0400

    Add Elixir `bob` solution

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,18 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-
+      question?(input) -> "Sure."
+      shouting?(input) -> "Whoa, chill out!"
+      silence?(input) -> "Fine. Be that way!"
+      :else -> "Whatever."
     end
+  end
+
+  defp question?(input), do: String.ends_with?(input, "?")
+
+  defp silence?(input), do: String.strip(input) == ""
+
+  defp shouting?(input) do
+    String.upcase(input) == input and String.downcase(input) != input
   end
 end
