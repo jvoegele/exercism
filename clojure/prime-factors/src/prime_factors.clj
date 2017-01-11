3384b82ff54381b7bd1c0b1eeabc459f43aabb9a
Jason Voegele <jason.voegele@gmail.com>
2017-01-11 05:57:38 -0500

    Update Clojure `prime-factors` to use loop

--- clojure/prime-factors/src/prime_factors.clj
+++ clojure/prime-factors/src/prime_factors.clj
@@ -1,12 +1,12 @@
 (ns prime-factors)
 
-(defn- prime-factors [n candidate acc]
-  (cond
-    (< n candidate) acc
-    (zero? (rem n candidate))
+(defn of [number]
+  (loop [n number
+         candidate 2
+         acc []]
+    (cond
+      (< n candidate) acc
+      (zero? (rem n candidate))
       (recur (/ n candidate) candidate (conj acc candidate))
-    :else (recur n (inc candidate) acc)))
-
-(defn of [n]
-  (prime-factors n 2 []))
+      :else (recur n (inc candidate) acc))))
 
