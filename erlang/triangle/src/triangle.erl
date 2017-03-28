bfa0e585544e81da2d1197e9b0225b31d410a9d4
Jason Voegele <jason.voegele@gmail.com>
2017-03-28 19:14:41 -0400

    Update Erlang `triangle` to use pattern match instead of guard

--- erlang/triangle/src/triangle.erl
+++ erlang/triangle/src/triangle.erl
@@ -9,7 +9,7 @@ kind(A, B, C) when A =< 0; B =< 0; C =< 0 ->
     {error, "all side lengths must be positive"};
 kind(A, B, C) when A + B =< C; A + C =< B; B + C =< A ->
     {error, "side lengths violate triangle inequality"};
-kind(A, B, C) when A == B, B == C ->
+kind(A, A, A) ->
     equilateral;
 kind(A, B, C) when A == B; A == C; B == C ->
     isosceles;
