456fed90f3be246b6125f5e2d432866a2ff898ed
Jason Voegele <jason.voegele@gmail.com>
2017-03-28 19:09:05 -0400

    Add Erlang `triangle` exercise and solution

---
+++ erlang/triangle/src/triangle.erl
@@ -0,0 +1,17 @@
+-module(triangle).
+-export([test_version/0]).
+-export([kind/3]).
+
+test_version() ->
+  1.
+
+kind(A, B, C) when A =< 0; B =< 0; C =< 0 ->
+    {error, "all side lengths must be positive"};
+kind(A, B, C) when A + B =< C; A + C =< B; B + C =< A ->
+    {error, "side lengths violate triangle inequality"};
+kind(A, B, C) when A == B, B == C ->
+    equilateral;
+kind(A, B, C) when A == B; A == C; B == C ->
+    isosceles;
+kind(_A, _B, _C) ->
+    scalene.
