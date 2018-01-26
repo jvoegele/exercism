70a8228c1845e9f88d3515106cd89a2a71902764
Jason Voegele <jason.voegele@gmail.com>
2018-01-26 15:51:12 -0500

    Update solution to ECMAscript `space-age` exercise

--- ecmascript/space-age/space-age.js
+++ ecmascript/space-age/space-age.js
@@ -1,4 +1,4 @@
-function round10(number) {
+function round100(number) {
   return Math.round(number * 100) / 100;
 }
 
@@ -12,34 +12,34 @@ export default class SpaceAge {
   }
 
   onEarth() {
-    return round10(this.earthAge());
+    return round100(this.earthAge());
   }
 
   onMercury() {
-    return round10(this.earthAge() / 0.2408467);
+    return round100(this.earthAge() / 0.2408467);
   }
 
   onVenus() {
-    return round10(this.earthAge() / 0.61519726);
+    return round100(this.earthAge() / 0.61519726);
   }
 
   onMars() {
-    return round10(this.earthAge() / 1.8808158);
+    return round100(this.earthAge() / 1.8808158);
   }
 
   onJupiter() {
-    return round10(this.earthAge() / 11.862615);
+    return round100(this.earthAge() / 11.862615);
   }
 
   onSaturn() {
-    return round10(this.earthAge() / 29.447498);
+    return round100(this.earthAge() / 29.447498);
   }
 
   onUranus() {
-    return round10(this.earthAge() / 84.016846);
+    return round100(this.earthAge() / 84.016846);
   }
 
   onNeptune() {
-    return round10(this.earthAge() / 164.79132);
+    return round100(this.earthAge() / 164.79132);
   }
 }
