639ec8b5db58071a8cc996a123a73fced61bc37d
Jason Voegele <jason.voegele@gmail.com>
2019-01-10 15:39:43 -0500

    Update Elixir `beer-song` exercise and solution

--- elixir/beer-song/beer_song.exs
+++ elixir/beer-song/beer_song.exs
@@ -3,36 +3,37 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t
-  def verse(number) do
-    n = number - 1
-    String.capitalize(on_the_wall(n)) <> ", " <> bottles_of_beer(n) <> ".\n" <>
-    action(n) <> ", " <> on_the_wall(pred(n)) <> ".\n"
+  def verse(n) do
+    """
+    #{String.capitalize(on_the_wall(n))}, #{bottles_of_beer(n)}.
+    #{action(n)}, #{on_the_wall(pred(n))}.
+    """
   end
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
   """
   @spec lyrics(Range.t) :: String.t
-  def lyrics(range \\ 100..1) do
+  def lyrics(range \\ 99..0) do
     Enum.map_join(range, "\n", &verse/1)
   end
 
-  def on_the_wall(n), do: bottles_of_beer(n) <> " on the wall"
+  defp on_the_wall(n), do: "#{bottles_of_beer(n)} on the wall"
 
-  def bottles_of_beer(n), do: amount(n) <> " " <> bottles(n) <> " of beer"
+  defp bottles_of_beer(n), do: "#{amount(n)} #{bottles(n)} of beer"
 
-  def action(0), do: "Go to the store and buy some more"
-  def action(n), do: "Take " <> pronoun(n) <> " down and pass it around"
+  defp action(0), do: "Go to the store and buy some more"
+  defp action(n), do: "Take #{pronoun(n)} down and pass it around"
 
-  def pred(0), do: 99
-  def pred(n), do: n - 1
+  defp pred(0), do: 99
+  defp pred(n), do: n - 1
 
-  def amount(0), do: "no more"
-  def amount(n), do: Integer.to_string(n)
+  defp amount(0), do: "no more"
+  defp amount(n), do: Integer.to_string(n)
 
-  def bottles(1), do: "bottle"
-  def bottles(_n), do: "bottles"
+  defp bottles(1), do: "bottle"
+  defp bottles(_n), do: "bottles"
 
-  def pronoun(1), do: "it"
-  def pronoun(_n), do: "one"
+  defp pronoun(1), do: "it"
+  defp pronoun(_n), do: "one"
 end
