21763fe230b547566b846565e809ce0f52ab6243
Jason Voegele <jason.voegele@gmail.com>
2019-01-17 12:12:49 -0500

    Add Elixir `twelve-days` exercise and solution

---
+++ elixir/twelve-days/twelve_days.exs
@@ -0,0 +1,72 @@
+defmodule TwelveDays do
+  @doc """
+  Given a `number`, return the song's verse for that specific day, including
+  all gifts for previous days in the same line.
+  """
+  @spec verse(number :: integer) :: String.t()
+  def verse(number) do
+    "On the #{day(number)} day of Christmas my true love gave to me: #{gifts(number)}."
+  end
+
+  @doc """
+  Given a `starting_verse` and an `ending_verse`, return the verses for each
+  included day, one per line.
+  """
+  @spec verses(starting_verse :: integer, ending_verse :: integer) :: String.t()
+  def verses(starting_verse, ending_verse) do
+    starting_verse..ending_verse
+    |> Enum.map(&verse/1)
+    |> Enum.join("\n")
+  end
+
+  @doc """
+  Sing all 12 verses, in order, one verse per line.
+  """
+  @spec sing() :: String.t()
+  def sing do
+    verses(1, 12)
+  end
+
+  @days [
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+    "eleventh",
+    "twelfth"
+  ]
+
+  defp day(n), do: Enum.at(@days, n - 1)
+
+  @gifts [
+    "a Partridge in a Pear Tree",
+    "two Turtle Doves",
+    "three French Hens",
+    "four Calling Birds",
+    "five Gold Rings",
+    "six Geese-a-Laying",
+    "seven Swans-a-Swimming",
+    "eight Maids-a-Milking",
+    "nine Ladies Dancing",
+    "ten Lords-a-Leaping",
+    "eleven Pipers Piping",
+    "twelve Drummers Drumming"
+  ]
+
+  defp gifts(day) when is_integer(day) do
+    @gifts
+    |> Enum.slice(0..(day - 1))
+    |> insert_and()
+    |> Enum.reverse()
+    |> Enum.join(", ")
+  end
+
+  defp insert_and([_] = list), do: list
+  defp insert_and([first | rest]), do: ["and " <> first | rest]
+end
